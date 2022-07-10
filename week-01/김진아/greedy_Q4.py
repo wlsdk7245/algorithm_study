b1d566b185f472a12e3ee4d63d0deda37e7376be
jina <wlsdk7245@gmail.com>
2022-07-10 22:11:32 +0900

    week1 Q4

---
+++ week-01/김진아/greedy_Q4.py
@@ -0,0 +1,15 @@
+N = int(input())
+dataSet = list(map(int, input().split()))
+dataSet.sort()
+
+group = 0
+cnt = 0
+
+for i in dataSet:
+    group += 1
+    if group >= i:
+        cnt += 1
+        group = 0
+
+print(dataSet)
+
