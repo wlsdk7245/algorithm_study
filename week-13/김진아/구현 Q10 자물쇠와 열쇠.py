bfe8cbd55ad4a58a2cb0948ac6bed5acf87d70a4
jina <wlsdk7245@gmail.com>
2022-11-08 21:41:19 +0900

    feat: 자물쇠와 열쇠 코드

--- week-13/김진아/구현 Q10 자물쇠와 열쇠.py
+++ week-13/김진아/구현 Q10 자물쇠와 열쇠.py
@@ -1,24 +1,3 @@
-def attach(i, j, m, rotatedKey, locks):
-    for a in range(m):
-        for b in range(m):
-            locks[a + i][b + j] += rotatedKey[a][b]
-
-
-def check(locks, m, n):
-    for i in range(n):
-        for j in range(n):
-            if locks[m + i][m + j] != 1:
-                return False
-    return True
-
-
-
-def detach(i, j, m, rotatedKey, locks):
-    for a in range(m):
-        for b in range(m):
-            locks[a + i][b + j] -= rotatedKey[a][b]
-
-
 def rotate(arr):
     n = len(arr)
     result = [[0] * n for _ in range(n)]
@@ -28,24 +7,41 @@ def rotate(arr):
     return result
 
 
+def check(arr):
+    n = len(arr) // 3
+
+    for i in range(n, n * 2):
+        for j in range(n, n * 2):
+            if arr[i][j] != 1:
+                return False
+    return True
+
+
 def solution(key, lock):
     m = len(key)
     n = len(lock)
 
     locks = [[0] * (n * 3) for _ in range(n * 3)]
 
+    # 자물쇠 새로운 배열에 저장
     for i in range(n):
         for j in range(n):
             locks[n + i][n + j] = lock[i][j]
 
+    # 열쇠를 동서남북 네 방향 회전시키며 하나씩 확인
     for _ in range(4):
+        key = rotate(key)
         for i in range(1, m + n):
             for j in range(1, m + n):
-                attach(i, j, key, locks)
-                if check(locks, m, n):
-                    return True
-                detach(i, j, key, locks)
-        key = rotate(key)
+                for x in range(m):
+                    for y in range(m):
+                        locks[x + i][y + j] += key[x][y]
 
+                if check(locks):
+                    return True
+
+                for x in range(m):
+                    for j in range(m):
+                        locks[x + i][y + j] -= key[x][y]
 
     return False
